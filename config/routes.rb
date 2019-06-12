acab744611adf81e450738ec33b43758ae42c9e3
Non Kositkittiwanit <smartnon2009@hotmail.com>
2019-06-12 11:45:18 +0700

    Delete unused controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tweets do
+  resources :tweets, only: [:index, :create, :show, :destroy] do
     resources :likes, module: :tweets, only: [:create, :destroy]
     resources :retweets, module: :tweets, only: [:new, :create]
-    resources :replies, module: :tweets, only: [:new, :create]
+    resources :replies, module: :tweets, only: [:create]
   end
 
   root 'tweets#index'
