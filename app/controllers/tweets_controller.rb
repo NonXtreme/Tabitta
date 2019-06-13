c280cfc936803be5a6b747eda66d0c11225016ce
Non Kositkittiwanit <smartnon2009@hotmail.com>
2019-06-13 10:56:55 +0700

    Sort the replies

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -15,7 +15,7 @@ class TweetsController < ApplicationController
   def show
     @tweet = Tweet.find_by(id: params[:id])
     if @tweet.present?
-      @replies = Tweet.where(reply_id: params[:id])
+      @replies = Tweet.where(reply_id: params[:id]).order(:created_at)
       @reply = Tweet.new
       if @has_retweet=@tweet.retweet_id.present?
         @retweet = Tweet.find_by(id: @tweet.retweet_id)
