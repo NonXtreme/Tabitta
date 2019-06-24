f3bf181c43e1da936f03857252cfd4f2a8f9d2c0
Non Kositkittiwanit <smartnon2009@hotmail.com>
2019-06-24 14:14:53 +0700

    Add username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :set_hashtags
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_hashtags
     @hashtags = HashtagTweet.joins(:hashtag).group(:hashtag).count.sort_by{|h| h[1]}.reverse
   end
+
+  protected
+
+      def configure_permitted_parameters
+          devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+
+          devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+      end
 end
