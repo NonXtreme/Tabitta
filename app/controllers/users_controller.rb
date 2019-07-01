4ffd070517734e7fb9bfcd1bbbc723858a47df3d
Non Kositkittiwanit <smartnon2009@hotmail.com>
2019-07-01 11:56:06 +0700

    See user followers and following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,30 @@
 
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(:followees, :followers).find_by(id: params[:id])
+    unless @user.present?
+      redirect_to tweets_path
+      return
+    end
     @tweets = @user.tweets.includes({retweet: [:likes, :user]}, {reply: :user} , :user, :likes).order(created_at: :desc).page params[:page]
     @follow = Following.find_by(follower_id: current_user&.id, followee_id: params[:id])
+  end
+
+  def followers
+    @user = User.includes(:followers).find_by(id: params[:user_id])
+    unless @user.present?
+      redirect_to tweets_path
+      return
+    end
+    @followers = @user.followers
+  end
+
+  def followees
+    @user = User.includes(:followees).find_by(id: params[:user_id])
+    unless @user.present?
+      redirect_to tweets_path
+      return
+    end
+    @followees = @user.followees
   end
 end
